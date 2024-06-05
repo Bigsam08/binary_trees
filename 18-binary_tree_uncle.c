341839d0606e7626ec706580fa35dfe3c7d52d97
Bigsam08 <uelsam994@gmail.com>
2024-06-05 13:43:43 -0700

    uncle err

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -10,7 +10,7 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node->parent || !node || node->parent->parent == NULL)
+	if (!node || !node->parent || node->parent->parent == NULL)
 		return (NULL);
 
 	if (node->parent->parent->left == node->parent)
