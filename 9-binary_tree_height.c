7d414077d92e8aa5fc2fc4331ffbf20789aad1a6
Bigsam08 <uelsam994@gmail.com>
2024-06-05 05:28:05 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t right = 0;
-    size_t left = 0, height;
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-    if (tree == NULL)
-    return (0);
-    left  = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    right = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-    height = ((left > right) ? 1 : right);
-    return (height);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
